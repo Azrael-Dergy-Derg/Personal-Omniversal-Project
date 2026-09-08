640e99a281b1384c5ec07333cff72cb7bc8a19d3
Azrael-Dergy-Derg <azrael8338@gmail.com>
2026-09-07 21:05:15 -0400

    Create __init__.py

---
+++ 04-Visual-Systems/OIPM/src/oipm/composition/__init__.py
@@ -0,0 +1,11 @@
+"""Composition and Camera Engine package for OIPM."""
+
+from .composition_engine import CompositionEngine, CompositionResult
+
+__all__ = [
+
+    "CompositionEngine",
+
+    "CompositionResult",
+
+]
