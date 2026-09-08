8a695191d776541ce205b8f7130317c5bc4f4b18
Azrael-Dergy-Derg <azrael8338@gmail.com>
2026-09-08 15:23:01 -0400

    Create __init__.py

---
+++ 04-Visual-Systems/OIPM/src/oipm/lighting/__init__.py
@@ -0,0 +1,11 @@
+"""Lighting and Atmosphere Engine package for OIPM."""
+
+from .lighting_engine import LightingEngine, LightingResult
+
+__all__ = [
+
+    "LightingEngine",
+
+    "LightingResult",
+
+]
