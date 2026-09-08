8abb781b6a839d518b69e969e43c445e51127bd3
Azrael-Dergy-Derg <azrael8338@gmail.com>
2026-09-07 21:02:59 -0400

    Create __init__.py

---
+++ 04-Visual-Systems/OIPM/src/oipm/scene/__init__.py
@@ -0,0 +1,11 @@
+"""Scene Construction Engine package for OIPM."""
+
+from .scene_constructor import SceneConstructionResult, SceneConstructor
+
+__all__ = [
+
+    "SceneConstructionResult",
+
+    "SceneConstructor",
+
+]
